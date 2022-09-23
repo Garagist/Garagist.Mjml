d5ceaa28b8348bfbf027abcd8feb100ef1347f04
Jon Uhlmann <account@uhlmann.pro>
2022-09-23 14:09:17 +0200

    New: Parse mj attributes from settings This can give mj-classes like in TailwindCSS

--- Classes/Eel/MjmlHelper.php
+++ Classes/Eel/MjmlHelper.php
@@ -18,6 +18,55 @@ class MjmlHelper implements ProtectedContextAwareInterface
     protected $apiService;
 
     /**
+     * @var array
+     * @Flow\InjectConfiguration(path="theme")
+     */
+    protected $themeConfiguration;
+
+    /**
+     * Return a theme setting
+     *
+     * @param string $path
+     * @return string|null
+     */
+    public function theme(string $path): ?string
+    {
+        $parts = explode('.', $path);
+        $lastPart = array_pop($parts);
+        $config = $this->themeConfiguration;
+        foreach ($parts as $part) {
+            $config = $this->getThemeValue($config, $part, false);
+        }
+        $config = $this->getThemeValue($config, $lastPart, true);
+        if (is_array($config) || $config == null || $config == '') {
+            return null;
+        }
+        return (string) $config;
+    }
+
+    /**
+     * Try to get the theme value, if not found, return the default value (if set)
+     *
+     * @param array $config
+     * @param string $part
+     * @param bool $isLast
+     * @return mixed
+     */
+    protected function getThemeValue(array $config, string $part, $isLast = false)
+    {
+        if (!array_key_exists($part, $config)) {
+            return null;
+        }
+        $config = $config[$part];
+
+        if ($isLast && is_array($config) && array_key_exists('DEFAULT', $config)) {
+            return $config['DEFAULT'];
+        }
+
+        return $config;
+    }
+
+    /**
      * Compile the html with MJML
      *
      * @param string $html
