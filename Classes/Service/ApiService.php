09a3ca28677c7b6cca202c30b6744e90aa164e71
Jon Uhlmann <account@uhlmann.pro>
2022-09-25 19:54:13 +0200

    New: Ability to use official API

--- Classes/Service/ApiService.php
+++ Classes/Service/ApiService.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Garagist\Mjml\Service;
 
-use Exception;
+use Neos\Flow\Exception;
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\RequestOptions;
+use GuzzleHttp\Exception\ClientException;
 use Neos\Flow\Annotations as Flow;
 use Psr\Log\LoggerInterface;
 use function sprintf;
@@ -39,20 +40,50 @@ class ApiService
     public function compileMjml(string $mjml, ?string $url = null): string
     {
         $client = new Client();
+        $endpoint = $this->configuration['apiEndpoint'];
+        $html = '';
 
         try {
-            $response = $client->request(
-                'POST',
-                $this->configuration['apiEndpoint'],
-                [
-                    'headers' => [
-                        'Content-Type' => 'text/plain'
-                    ],
-                    'body' => $mjml,
-                ]
-            );
-        } catch (GuzzleException $exception) {
-            $message = $exception->getMessage();
+            if ($endpoint === true || str_starts_with($endpoint, 'https://api.mjml.io/')) {
+                $response = $client->request(
+                    'POST',
+                    'https://api.mjml.io/v1/render',
+                    [
+                        RequestOptions::HEADERS => [
+                            'Content-Type' => 'application/x-www-form-urlencoded',
+                            'Accepts' => 'application/json',
+                        ],
+                        RequestOptions::AUTH => [
+                            $this->configuration['applicationID'],
+                            $this->configuration['secretKey']
+                        ],
+                        RequestOptions::JSON => [
+                            'mjml' => $mjml,
+                        ],
+                    ]
+                );
+                $contents = $response->getBody()->getContents();
+                $json = json_decode($contents, true);
+                $html = $json['html'];
+            } else {
+                $response = $client->request(
+                    'POST',
+                    $endpoint,
+                    [
+                        RequestOptions::HEADERS => [
+                            'Content-Type' => 'text/plain'
+                        ],
+                        RequestOptions::AUTH => [
+                            $this->configuration['applicationID'],
+                            $this->configuration['secretKey']
+                        ],
+                        RequestOptions::BODY => $mjml,
+                    ]
+                );
+                $html = $response->getBody();
+            }
+        } catch (ClientException $exception) {
+            $message = $exception->getResponse()->getBody()->getContents();
 
             $logMessage = 'Calling MJML API ';
             if (isset($url)) {
@@ -61,7 +92,7 @@ class ApiService
             $logMessage .= sprintf('failed with reason: %s', $message);
             $this->logger->error($logMessage);
 
-            return $message;
+            throw new Exception($message, 1664127843);
         }
 
         $logMessage = 'Successfully called MJML API';
@@ -70,6 +101,6 @@ class ApiService
         }
 
         $this->logger->debug($logMessage);
-        return (string) $response->getBody();
+        return (string) $html;
     }
 }
